a2f0f066afbcfeee9438e86a37ea1c3e0d9a838c
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 11:28:24 +0300

    Attempting to solve the power_two problem

---
+++ Power_of_two.rb
@@ -0,0 +1,8 @@
+Input is an integer, out put is also an integer.
+Idea is to input a number that should return true if the number is a power of 2 and false if otherwise.
+powers of: 2*2 = 4, 2*2*2 = 8, 2*2*2*2 = 16, 2*2*2*2*2 = 32.....
+
+def is_power_of_two?(num)
+	
+	
+end
