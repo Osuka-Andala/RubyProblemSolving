6c3936ce6a072165aa06f26afc384854f7b54ce9
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 15:25:09 +0300

    Attempting to solve the most_common_letter problem

---
+++ Most_common_letter.rb
@@ -0,0 +1,6 @@
+# Input is astring and output is a string and an integer.
+# Plan is to take a sting and identify the most common letter and also give us a count of the number of times it appears.
+
+def most_commom_letter(string)
+	
+end
