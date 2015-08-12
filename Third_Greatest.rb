ff4952ebd2abeb3321663b26ac40fc82b8ec4718
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 14:46:50 +0300

    The third_greatets problem, solved

--- Third_Greatest.rb
+++ Third_Greatest.rb
@@ -4,5 +4,13 @@
 
 
 def third_greatest(nums)
- 	
+
+  # nums = []
+
+  nums.sort[-3]
+
  end 
+
+puts('third_greatest([5, 3, 7]) == 3: ' + (third_greatest([5, 3, 7]) == 3).to_s)
+puts('third_greatest([5, 3, 7, 4]) == 4: ' + (third_greatest([5, 3, 7, 4]) == 4).to_s)
+puts('third_greatest([2, 3, 7, 4]) == 3: ' + (third_greatest([2, 3, 7, 4]) == 3).to_s)
