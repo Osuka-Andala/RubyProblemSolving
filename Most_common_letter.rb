2a34d81ae08ed05621c9855ec83bceb4e290242f
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 19:59:44 +0300

    Failed both tests

--- Most_common_letter.rb
+++ Most_common_letter.rb
@@ -1,6 +1,20 @@
 # Input is astring and output is a string and an integer.
 # Plan is to take a sting and identify the most common letter and also give us a count of the number of times it appears.
 
-def most_commom_letter(string)
+def most_common_letter(string)
+
+    string_arr = []
+
+    
+	string.each_char do |x|
+		y = string.count(x)
+           string_arr.push(y)    
+	         end
+
+	         pointer = string_arr.index(string_arr.max)
+	         return [[pointer], string_arr.max]
 	
 end
+
+puts('most_common_letter("abca") == ["a", 2]: ' + (most_common_letter('abca') == ['a', 2]).to_s)
+puts('most_common_letter("abbab") == ["b", 3]: ' + (most_common_letter('abbab') == ['b', 3]).to_s)
