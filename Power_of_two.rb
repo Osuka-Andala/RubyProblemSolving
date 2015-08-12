4ac62d36e340862ad806d341dcaea72169049212
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 11:46:29 +0300

    Passed test 4 and 5, failed test 1, 2 and 3

--- Power_of_two.rb
+++ Power_of_two.rb
@@ -1,8 +1,26 @@
-Input is an integer, out put is also an integer.
-Idea is to input a number that should return true if the number is a power of 2 and false if otherwise.
-powers of: 2*2 = 4, 2*2*2 = 8, 2*2*2*2 = 16, 2*2*2*2*2 = 32.....
+# Input is an integer, out put is also an integer.
+# Idea is to input a number that should return true if the number is a power of 2 and false if otherwise.
+# powers of: 2*2 = 4, 2*2*2 = 8, 2*2*2*2 = 16, 2*2*2*2*2 = 32.....
 
 def is_power_of_two?(num)
-	
+	num = 0
+	i = 0
+
+	if (2 ** i == num)  && (Math.log(num)/Math.log(2) == i) 
+		return true
+
+		i += 1
+		num += 1
+
+	end
+
+	return false
+
 	
 end
+
+puts('is_power_of_two?(1) == true: ' + (is_power_of_two?(1) == true).to_s)
+puts('is_power_of_two?(16) == true: ' + (is_power_of_two?(16) == true).to_s)
+puts('is_power_of_two?(64) == true: ' + (is_power_of_two?(64) == true).to_s)
+puts('is_power_of_two?(78) == false: ' + (is_power_of_two?(78) == false).to_s)
+puts('is_power_of_two?(0) == false: ' + (is_power_of_two?(0) == false).to_s)
