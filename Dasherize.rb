3f1739d97a6cd439c40aa5be33d068a035453809
Osuka-Andala <teresa.andala@gmail.com>
2015-08-12 17:43:37 +0300

    Attempting to solve the dasherize problem

---
+++ Dasherize.rb
@@ -0,0 +1,8 @@
+# Input is an integer and out put is a string.
+# The plan is to put a dash before and after each odd digit without starting the string with a dash.
+
+
+
+def dasherize_number(num)
+	
+end
