763d2ca7598e543f123f16a53340b6d5ca6ddb1b
Osuka-Andala <teresa.andala@gmail.com>
2015-08-13 16:03:41 +0300

    Returned false on all tests

--- Dasherize.rb
+++ Dasherize.rb
@@ -4,5 +4,28 @@
 
 
 def dasherize_number(num)
+
+    num.odd?
+
+	num.to_s.each_char do |x| x.insert(-1, '-')
+	end
+
 	
-end
+end 
+
+puts(
+  'dasherize_number(203) == "20-3": ' +
+  (dasherize_number(203) == '20-3').to_s
+)
+puts(
+  'dasherize_number(303) == "3-0-3": ' +
+  (dasherize_number(303) == '3-0-3').to_s
+)
+puts(
+  'dasherize_number(333) == "3-3-3": ' +
+  (dasherize_number(333) == '3-3-3').to_s
+)
+puts(
+  'dasherize_number(3223) == "3-22-3": ' +
+  (dasherize_number(3223) == '3-22-3').to_s
+)
