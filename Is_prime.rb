54dfb9d2e0d6d16f5111d70e33e5ba58e5050974
Osuka-Andala <teresa.andala@gmail.com>
2015-08-13 19:48:09 +0300

    First attempt, test 1 and 2 passed. test 3 and 4 failed

--- Is_prime.rb
+++ Is_prime.rb
@@ -2,6 +2,17 @@
 # should be greater than one and return true if it is a prime number and false if otherwise.
 
 
-def is_prime(number)
-	
+def is_prime?(number)
+
+	i = 0
+	if (number % number == 0) && (number % 1 == 0)
+
+		return true
+		end
+		return false
 end
+
+puts('is_prime?(2) == true: ' + (is_prime?(2) == true).to_s)
+puts('is_prime?(3) == true: ' + (is_prime?(3) == true).to_s)
+puts('is_prime?(4) == false: ' + (is_prime?(4) == false).to_s)
+puts('is_prime?(9) == false: ' + (is_prime?(9) == false).to_s)
