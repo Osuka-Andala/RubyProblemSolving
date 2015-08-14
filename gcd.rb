133dd5e48d3ebb8ad7fa452f9ed328638325485f
Osuka-Andala <teresa.andala@gmail.com>
2015-08-14 11:16:28 +0300

    Attempting to solve the gcd problem

---
+++ gcd.rb
@@ -0,0 +1,7 @@
+Input is an integer and output is also an integer.
+Plan is to take two numbers and return their gcd, greatest common divisor.
+
+
+def greatest_common_factor(number1, number2)
+	
+end
